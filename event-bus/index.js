c74417020c103d601e9c719a50a03be981f90bef
rezanicgil <rezanicgil@gmail.com>
2023-04-30 16:32:08 +0300

    feature: query servie configured

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,6 +10,8 @@ app.post('/events', (req, res) => {
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:5000/events', event);
+    axios.post('http://localhost:4002/events', event);
+
 
     res.send({ status:'OK' })
 
