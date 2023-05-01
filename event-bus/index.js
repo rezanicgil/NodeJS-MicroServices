870a00eb861c56f3a1564179356c146c820e1f50
rezanicgil <rezanicgil@gmail.com>
2023-05-01 10:41:16 +0300

    feat: moderation effect emitted on frontend app

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,14 +6,24 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
+
+try{
+
+
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:5000/events', event);
     axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
+
 
 
     res.send({ status:'OK' })
+
+}catch(error) {
+    console.log(error);
+}
 
 });
 
