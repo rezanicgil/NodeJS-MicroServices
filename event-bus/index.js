f5d5efa56cde6c648ca9b4cefbd35049e8a80e38
rezanicgil <rezanicgil@gmail.com>
2023-05-01 12:07:47 +0300

    feat: event syncing in action

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,13 +4,15 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
-
+const events = [];
 app.post('/events', (req, res) => {
 
 try{
 
 
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:5000/events', event);
@@ -27,6 +29,9 @@ try{
 
 });
 
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, ()=>{
     console.log('Listening on 4005');
